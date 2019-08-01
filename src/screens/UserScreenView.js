45de15ba4077b04c28ec7c5721333eb8a0e83912
tuzmusic <tuzmusic@gmail.com>
2019-08-01 10:04:55 -0400

    add logo and info contact to user page

--- src/screens/UserScreenView.js
+++ src/screens/UserScreenView.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, Linking } from "react-native";
+import {
+  View,
+  Text as BaseText,
+  TouchableOpacity,
+  Linking,
+  ScrollView
+} from "react-native";
 import { connect } from "react-redux";
 import { logout } from "../redux/actions/authActions";
 import UserProfile from "../subviews/UserProfile";
+import { Divider, Image } from "react-native-elements";
 
 class UserScreen extends Component {
   UNSAFE_componentWillReceiveProps(newProps) {
@@ -11,46 +18,58 @@ class UserScreen extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.container}>
         {this.props.user && (
           <UserProfile
             user={this.props.user}
             onLogout={this.props.logout.bind(this)}
           />
         )}
-        <View style={styles.bottomSection}>
-          <Text style={[styles.text]}>Please</Text>
-          <TouchableOpacity
-            style={styles.opacity}
-            onPress={() =>
-              Linking.openURL("http://joinelectro.com/submit-listings/")
-            }
-          >
-            <Text style={[styles.text, styles.link]}>visit the website</Text>
-          </TouchableOpacity>
-          <Text style={[styles.text]}>to create a station.</Text>
+        <Image
+          source={require("../../assets/logos/ElectroLogo.png")}
+          style={styles.image}
+        />
+        <View>
+          <View style={styles.rowText}>
+            <Text>To create a station, please </Text>
+            <Link url="http://joinelectro.com/submit-listings/">
+              visit our website
+            </Link>
+            <Text>.</Text>
+          </View>
+          <View style={styles.rowText}>
+            <Text>For questions, email </Text>
+            <Link url="mailto:info@joinelectro.com">info@joinelectro.com</Link>
+            <Text style={styles.text}>.</Text>
+          </View>
         </View>
-      </View>
+      </ScrollView>
     );
   }
 }
 
+const Text = props => (
+  <BaseText style={[styles.text, props.style]}>{props.children}</BaseText>
+);
+
+const Link = props => (
+  <TouchableOpacity onPress={() => Linking.openURL(props.url)}>
+    <Text style={[styles.text, styles.link]}>{props.children}</Text>
+  </TouchableOpacity>
+);
+
 export default connect(
-  state => {
-    return { user: state.auth.user, isLoading: state.auth.isLoading };
-  },
+  ({ auth: { user, isLoading } }) => ({ user, isLoading }),
   { logout }
 )(UserScreen);
 
 const styles = {
   container: {
-    flex: 1,
     justifyContent: "flex-start",
     alignItems: "center"
   },
-  topSection: { width: "100%", alignItems: "center" },
-  button: { width: "65%", marginTop: 30 },
-  logout: { backgroundColor: "red" },
-  text: { fontSize: 24, textAlign: "center" },
+  image: { height: 110, width: 110, resizeMode: "contain", marginVertical: 15 },
+  text: { fontSize: 18, textAlign: "center" },
+  rowText: { flexDirection: "row", marginBottom: 5 },
   link: { color: "blue", textDecorationLine: "underline" }
 };
